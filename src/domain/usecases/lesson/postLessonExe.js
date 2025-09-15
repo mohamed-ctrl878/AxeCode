0ac90a96bf3a3d0067cc682969a5a84969d3b695
mohamed mahmoud <bnaboabdallah@gmail.com>
2025-09-15 22:11:39 +0300

    before finish upload Lesson& Course & media over all

--- src/domain/usecases/lesson/postLessonExe.js
+++ src/domain/usecases/lesson/postLessonExe.js
@@ -1,13 +1,12 @@
-import { toFromData } from "@core/utils/problemUploader/handellers";
+import { toFormData } from "@core/utils/problemUploader/handellers";
+import { PostMedia } from "@data/repositories/media/PostMedia";
 
-export const postLessonExe = async (
-  mediaCore,
-  mediaData,
-  lessonCore,
-  dataLesson
-) => {
-  const dataToSend = await mediaCore.postContent(toFromData(mediaData));
-  const PostingLesson = await lessonCore.postContent({
+export const postLessonExe = async (core, dataLesson) => {
+  console.log("dataLesson", dataLesson);
+  const dataToSend = await new PostMedia().postContent(
+    toFormData(dataLesson.video)
+  );
+  const PostingLesson = await core.postContent({
     ...dataLesson,
     video: dataToSend.id,
   });
